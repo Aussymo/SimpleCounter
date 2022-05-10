e7b52d0f44997094f6fa810c278a41b7cafc1c31
Aussymo <aussymo@gmail.com>
2022-05-10 03:22:34 +0000

    Created Repo

--- src/js/component/Timer.js
+++ src/js/component/Timer.js
@@ -2,9 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export const Timer = (props) => {
-	let placeholder = "00000000";
+	let placeholder = "000000";
 
 	let seconds = props.seconds.toString();
+	let watch = <i className="fa-solid fa-stopwatch"></i>;
 
 	let output = (
 		placeholder.split("").slice(seconds.length).join("") + seconds
@@ -12,6 +13,7 @@ export const Timer = (props) => {
 
 	return (
 		<div className="clock">
+			<i className="far fa-hourglass"></i>
 			{output.map((item, i) => {
 				return (
 					<div className="time" key={i}>
